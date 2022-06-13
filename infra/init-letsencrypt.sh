2c54e62451cea104135ea8920cc72b9c18ca7c52
Glaser1 <sashapliskov@gmail.com>
2022-06-13 04:50:14 +0300

    Update init-letsencrypt.sh

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(foodgram-project.ddns.net www.foodgram-project.ddns.net)
+domains=(foodgram-project.ddns.net 51.250.108.12)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sashapliskov@gmail.com" # Adding a valid address is strongly recommended
